bb6f2126c0d04598e278329dfa25f82213b158a8
Luke Armstrong <lukea@propcom.co.uk>
2012-03-29 14:34:39 +0100

    updated the docblox

--- bootstrap.php
+++ bootstrap.php
@@ -1,15 +1,25 @@
 <?php
+
 /**
- * Fuel
+ * is_email (FuelPHP Package)
  *
- * Fuel is a fast, lightweight, community driven PHP5 framework.
+ * This allows you to use the is_email function with the Validation class in the FuelPHP Framework.
  *
- * @package    Fuel
- * @version    1.0
- * @author     Fuel Development Team
- * @license    MIT License
- * @copyright  2010 - 2012 Fuel Development Team
- * @link       http://fuelphp.com
+ * @package		is_email (FuelPHP Package)
+ * @version		1.0
+ * @author		Luke Armstrong <lukea@propcom.co.uk>
+ * @copyright	2012 Propeller Communications
+ * @license		http://www.opensource.org/licenses/bsd-license.php BSD License
+ * @link		https://github.com/propcom/fuel-is_email/
+ */
+
+/**
+ * @package     is_email
+ * @version		3.01.1 - Fixed examples and readme.txt
+ * @author      Dominic Sayers <dominic@sayers.cc>
+ * @copyright	2008-2011 Dominic Sayers
+ * @license		http://www.opensource.org/licenses/bsd-license.php BSD License
+ * @link		http://www.dominicsayers.com/isemail
  */
 
 require_once __DIR__.'/vendor/is_email/is_email.php';
@@ -20,5 +30,3 @@ Autoloader::add_classes(array(
 	'Is_Email\\Validation' => __DIR__.'/classes/validation.php',
 ));
 
-
-/* End of file bootstrap.php */
